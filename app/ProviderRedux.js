3ba87833c3d178c0f3f757cf9275a462bdcf4faf
Michael Wood <michaelwood33311@icloud.com>
2023-07-01 09:42:05 -0400

    refactored socket connection setup for readability

--- app/ProviderRedux.js
+++ app/ProviderRedux.js
@@ -1,13 +1,14 @@
 'use client';
-    
 import { Provider } from "react-redux";
 import { store } from "../store/configStore";
 
 import { io } from 'socket.io-client';
 import * as socketService from './socketService';
 
+const socketURL = `https://echo.instantchatbot.net:6260`;
+
 console.log('hello there');
-socketService.setupTheSocket(io, `https://echo.instantchatbot.net:6260`, store);
+socketService.setupTheSocket(io, socketURL, store);
 
 export function ProviderRedux({ children }) {
     return (
